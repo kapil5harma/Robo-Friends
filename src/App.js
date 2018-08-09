50278499d8efdd884d9f2b3476d5faf444553435
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-08-09 18:16:39 +0530

    Imported Sega Logo font for App heading and a bit of styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { robots } from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import './App.css';
 
 class App extends Component {
   state = {
@@ -24,7 +25,7 @@ class App extends Component {
     // console.log('filteredRobots: ', filteredRobots);
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
