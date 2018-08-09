c0c9c401a53dcb1c8317c8363c8ab79bbbbb4682
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-08-09 13:32:43 +0530

    CardList component

---
+++ src/CardList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  const cardComponent = robots.map((robot, i) => {
+    console.log('robot: ', robot);
+    return (
+      <Card
+        key={robot.id}
+        id={robot.id}
+        name={robot.name}
+        email={robot.email}
+      />
+    );
+  });
+  return <div>{cardComponent}</div>;
+};
+
+export default CardList;
