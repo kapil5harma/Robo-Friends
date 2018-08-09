f7807da8f48b60134e6748a3a19b2dbca0b78474
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-08-09 19:35:29 +0530

    ErrorBoundary container created and used

---
+++ src/containers/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch = (error, info) => {
+    this.setState({ hasError: true });
+  };
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Ooops! That is not good!</h1>;
+    } else {
+      return this.props.children;
+    }
+  }
+}
+
+export default ErrorBoundary;
