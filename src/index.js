8c6a71d63f8a1748ff11e23e219ed5fd8667161e
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-08-11 21:24:05 +0530

    npm i --save redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+
 import { createLogger } from 'redux-logger';
+import thunk from '../node_modules/redux-thunk';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
@@ -10,7 +12,7 @@ import App from './containers/App';
 import { searchRobots } from './reducers';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(searchRobots, applyMiddleware(thunk, logger));
 
 const app = (
   <Provider store={store}>
