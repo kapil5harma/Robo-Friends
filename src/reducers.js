63bac88dc2d49b9bf50762656928d696995acd81
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-08-12 20:02:46 +0530

    requestRobots reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,15 +1,45 @@
-import { CHANGE_SEARCH_FIELD } from './actionTypes';
+import * as actionTypes from './actionTypes';
 
-const initialState = {
+const initialStateSearch = {
   searchField: ''
 };
 
-export const searchRobots = (state = initialState, action = {}) => {
+export const searchRobots = (state = initialStateSearch, action = {}) => {
   switch (action.type) {
-    case CHANGE_SEARCH_FIELD:
+    case actionTypes.CHANGE_SEARCH_FIELD:
       return {
         ...state,
         searchField: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+const initialStateRobots = {
+  loading: false,
+  robots: [],
+  error: ''
+};
+
+export const requestRobots = (state = initialStateRobots, action = {}) => {
+  switch (action.type) {
+    case actionTypes.REQUEST_ROBOTS_START:
+      return {
+        ...state,
+        loading: true
+      };
+    case actionTypes.REQUEST_ROBOTS_SUCCESS:
+      return {
+        ...state,
+        robots: action.payload.robots,
+        loading: false
+      };
+    case actionTypes.REQUEST_ROBOTS_FAILED:
+      return {
+        ...state,
+        error: action.payload.error,
+        loading: false
       };
     default:
       return state;
