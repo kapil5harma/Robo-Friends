e5003c53546bc04a6c3a555ee987ef94c171f82d
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-08-12 20:03:22 +0530

    Combine reducers into rootReducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 
 import { createLogger } from 'redux-logger';
 import thunk from '../node_modules/redux-thunk';
@@ -9,10 +9,13 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import App from './containers/App';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(thunk, logger));
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 const app = (
   <Provider store={store}>
